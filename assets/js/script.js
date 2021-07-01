5f8f240f15ea2a490b833cd98d67d8ea8e649a54
AnaG0307 <ana.genover@gmail.com>
2021-07-01 13:10:41 +0000

    Create new version of roundOutcome function for better readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+// /* jshint esversion: 6 */
+
+// var basePage = require('./basePage.js');
+// var casual = require('casual');
+// var randomRut = '#######';
+// var randomPhone = '##########';
+
+
 // Variables to stablish the players, buttons with all answers to choose and outcome of the round
 
 let playerAnswerArea = document.getElementsByClassName('player-answer')[0];
@@ -13,14 +21,14 @@ let playerAnswer = [
     'spock'
 ];
 
-imagesPath = "assets/images/"
+imagesPath = "assets/images/";
 let gameImages = {
-    rock : imagesPath + "rock.jpg", 
-    lizard : imagesPath + "lizard.jpg",
-    paper : imagesPath + "paper.jpg",
-    scissors : imagesPath + "scissor.jpg",
-    spock : imagesPath + "spock.jpg"
-    };
+    rock: imagesPath + "rock.jpg",
+    lizard: imagesPath + "lizard.jpg",
+    paper: imagesPath + "paper.jpg",
+    scissors: imagesPath + "scissor.jpg",
+    spock: imagesPath + "spock.jpg"
+};
 
 // Create answer from player and show image for player and computer choices
 
@@ -29,8 +37,8 @@ buttonsAnswers.forEach(answer => answer.addEventListener('click', (occasion) => 
 
     returnedAnswer = createComputerAnswer();
 
-    let returnedPlayerImage =  getImage(playerAnswer);
-    playerAnswerArea.innerText = playerAnswer;  
+    let returnedPlayerImage = getImage(playerAnswer);
+    playerAnswerArea.innerText = playerAnswer;
     playerImage = getImage(playerAnswer);
 
     computerImage = getImage(returnedAnswer);
@@ -44,105 +52,99 @@ buttonsAnswers.forEach(answer => answer.addEventListener('click', (occasion) => 
 
 function getImage(playerAnswer) {
     return gameImages[playerAnswer];
-};
+}
 
 // Create answer from computer
 
 function createComputerAnswer() {
     let assignPosition = Math.floor(Math.random() * 5);
-    let answers = ['rock','paper','scissors','lizard','spock'];
+    let answers = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     return answers[assignPosition];
-};
+}
 
 
 // Generate an outcome by comparing player and computer´s answers
 
+// function roundOutcome() {
+//     if (playerAnswer === returnedAnswer) {
+//         outcome = "Nobody wins, try again!";
+//     };
+//     if (playerAnswer === 'rock' && returnedAnswer === 'paper') {
+//         outcome = 'You lost!';
+//     };
+//     if (playerAnswer === 'rock' && returnedAnswer === 'scissors') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'rock' && returnedAnswer === 'lizard') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'rock' && returnedAnswer === 'spock') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'paper' && returnedAnswer === 'rock') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'paper' && returnedAnswer === 'scissors') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'paper' && returnedAnswer === 'lizard') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'paper' && returnedAnswer === 'spock') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'scissors' && returnedAnswer === 'rock') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'scissors' && returnedAnswer === 'paper') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'scissors' && returnedAnswer === 'lizard') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'scissors' && returnedAnswer === 'spock') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'lizard' && returnedAnswer === 'rock') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'lizard' && returnedAnswer === 'paper') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'lizard' && returnedAnswer === 'scissors') {
+//         outcome = 'You lost!'
+//     };
+//     if (playerAnswer === 'lizard' && returnedAnswer === 'spock') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'spock' && returnedAnswer === 'rock') {
+//         outcome = 'You win!'
+//     };
+//     if (playerAnswer === 'spock' && returnedAnswer === 'paper') {
+//         outcome = 'You lost!';
+//     };
+//     if (playerAnswer === 'spock' && returnedAnswer === 'scissors') {
+//         outcome = 'You win!';
+//     };
+//     if (playerAnswer === 'spock' && returnedAnswer === 'lizard') {
+//         outcome = 'You lost!';
+//     };
+
+//     document.getElementById("outcome-reaction").innerHTML = outcome;
+//     console.log(outcome);
+// }
+
+
+
 function roundOutcome() {
+    let outcome;
     if (playerAnswer === returnedAnswer) {
         outcome = "Nobody wins, try again!";
-    };
-    if (playerAnswer === 'rock' && returnedAnswer === 'paper') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'rock' && returnedAnswer === 'scissors') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'rock' && returnedAnswer === 'lizard') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'rock' && returnedAnswer === 'spock') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'paper' && returnedAnswer === 'rock') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'paper' && returnedAnswer === 'scissors') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'paper' && returnedAnswer === 'lizard') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'paper' && returnedAnswer === 'spock') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'scissors' && returnedAnswer === 'rock') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'scissors' && returnedAnswer === 'paper') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'scissors' && returnedAnswer === 'lizard') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'scissors' && returnedAnswer === 'spock') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'lizard' && returnedAnswer === 'rock') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'lizard' && returnedAnswer === 'paper') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'lizard' && returnedAnswer === 'scissors') {
-        outcome = 'You lost!'
-    };
-    if (playerAnswer === 'lizard' && returnedAnswer === 'spock') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'spock' && returnedAnswer === 'rock') {
-        outcome = 'You win!'
-    };
-    if (playerAnswer === 'spock' && returnedAnswer === 'paper') {
-        outcome = 'You lost!';
-    };
-    if (playerAnswer === 'spock' && returnedAnswer === 'scissors') {
-        outcome = 'You win!';
-    };
-    if (playerAnswer === 'spock' && returnedAnswer === 'lizard') {
-        outcome = 'You lost!';
-    };
-    
+    } else if ((playerAnswer === 'rock' && returnedAnswer === 'scissors') || (playerAnswer === 'rock' && returnedAnswer === 'lizard') || (playerAnswer === 'paper' && returnedAnswer === 'rock') || (playerAnswer === 'paper' && returnedAnswer === 'spock') || (playerAnswer === 'scissors' && returnedAnswer === 'paper') || (playerAnswer === 'scissors' && returnedAnswer === 'lizard') || (playerAnswer === 'lizard' && returnedAnswer === 'paper') || (playerAnswer === 'lizard' && returnedAnswer === 'spock') || (playerAnswer === 'spock' && returnedAnswer === 'rock') || (playerAnswer === 'spock' && returnedAnswer === 'scissors')) {
+        outcome = "You win!";
+    } else if ((playerAnswer === 'rock' && returnedAnswer === 'paper') || (playerAnswer === 'rock' && returnedAnswer === 'spock') || (playerAnswer === 'paper' && returnedAnswer === 'scissors') || (playerAnswer === 'paper' && returnedAnswer === 'lizard') || (playerAnswer === 'scissors' && returnedAnswer === 'rock') || (playerAnswer === 'scissors' && returnedAnswer === 'spock') || (playerAnswer === 'lizard' && returnedAnswer === 'rock') || (playerAnswer === 'lizard' && returnedAnswer === 'scissors') || (playerAnswer === 'spock' && returnedAnswer === 'paper') || (playerAnswer === 'spock' && returnedAnswer === 'lizard')) {
+        outcome = "You lost!";
+    }
     document.getElementById("outcome-reaction").innerHTML = outcome;
     console.log(outcome);
 }
-
-// Get score updated with round results
-
-function score() {
-    let userPoints = 0;
-    let compPoints = 0;
-    let tPoints = 0;
-};
-
-function updatePoints() {
-    let playerPoints = document.getElementById('player-points p');
-    let compPoints = document.getElementById('computer-points p');
-    let tiePoints = document.getElementById('tie-points p');
-
-    playerPoints.innerHTML = userPoints;
-    compPoints.innerHTML = compPoints;
-    tiePoints.innerHTML = tPoints;
-};
-
-
-
